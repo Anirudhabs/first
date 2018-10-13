8cf9db495ba225c5031b6223bcc7f7c4f7211d1b
Anirudhs B S <32251710+anirudha-bs@users.noreply.github.com>
2018-10-13 20:57:09 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,25 @@
+import serial
+import time
+import pyautogui
+
+ArduinoSerial = serial.Serial('/dev/ttyACM1',9600)
+time.sleep()
+
+while 1:
+     out = str (ArduinoSerial.readline())
+    print (out)
+
+    if 'Play/Pause' in out:
+        pyautogui.typewrite(['space'], 0.2)
+
+    if '3' in out:
+        pyautogui.scroll(50)
+
+    if '4' in out:
+        pyautogui.scroll(-50)
+
+    if '1' in out:
+        pyautogui.press('right')
+
+    if '2' in out:
+        pyautogui.press('left')
---
+++ ultrasonics.ino
@@ -0,0 +1,95 @@
+
+const int trigger1 = 8; 
+const int echo1 = 9; 
+const int trigger2 = 7; 
+const int echo2 = 6;
+
+long time_taken;
+int dist,distL,distR;
+
+ 
+
+void setup() {
+Serial.begin(9600); 
+  
+pinMode(trigger1, OUTPUT); 
+pinMode(echo1, INPUT); 
+pinMode(trigger2, OUTPUT); 
+pinMode(echo2, INPUT); 
+}
+
+
+void calculate_distance(int trigger, int echo)
+{
+digitalWrite(trigger, LOW);
+delayMicroseconds(2);
+digitalWrite(trigger, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigger, LOW);
+
+time_taken = pulseIn(echo, HIGH);
+dist= time_taken*0.034/2;
+if (dist>50)
+dist = 50;
+}
+
+void loop() { 
+calculate_distance(trigger1,echo1);
+distL =dist; 
+
+calculate_distance(trigger2,echo2);
+distR =dist; 
+
+
+if ((distL >40 && distR>40) && (distL <50 && distR<50)) 
+{Serial.println("Play/Pause");delay(1000);}
+
+calculate_distance(trigger1,echo1);
+distL =dist;
+
+calculate_distance(trigger2,echo2);
+distR =dist;
+
+ 
+
+if (distL>=13 && distL<=17)
+{ 
+  calculate_distance(trigger1,echo1);
+  distL =dist;
+  if (distL>=13 && distL<=17)
+  {
+    Serial.println("Left Locked");
+    while(distL<=40)
+    {
+      calculate_distance(trigger1,echo1);
+      distL =dist;
+      if (distL<15)  
+      {Serial.println ("3");delay(1000);}
+      if (distL>25) 
+      {Serial.println ("4");delay(1000);}
+    }
+  }
+}
+
+
+if (distR>=13 && distR<=17)
+{ 
+  calculate_distance(trigger2,echo2);
+  distR =dist;
+  if (distR>=13 && distR<=17)
+  {
+    Serial.println("Right Locked");
+    while(distR<=40)
+    {
+      calculate_distance(trigger2,echo2);
+      distR =dist;
+      if (distR<15) 
+      {Serial.println ("1");delay(1000);}
+      if (distR>25) 
+      {Serial.println ("2");delay(1000);}
+  }
+}
+}
+
+delay(200);
+}
